c3dd24f03d32b55845cb433149cfe59a8813a50d
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-07-28 22:04:53 +0200

    Implement logging in KNN

--- models/artificial_clustering/KNN.py
+++ models/artificial_clustering/KNN.py
@@ -5,6 +5,9 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 
 from packages.path_utility import get_output_root_path
+from packages.logging import setup_base_logging
+
+logger = setup_base_logging()
 
 ### Constants ###
 N_SAMPLES = 1000    # Number of rows of the dataset
@@ -14,7 +17,7 @@ N_INIT = 15         # Times K-means will be run with different centroid seeds
 
 
 ### Setup ###
-print(f"Making blobs with {N_SAMPLES} rows, {N_FEATURES} columns and "
+logger.info(f"Making blobs with {N_SAMPLES} rows, {N_FEATURES} columns and "
         + f"{N_CENTERS} centers")
 dataset, _ = make_blobs(
     n_samples=N_SAMPLES,
@@ -25,27 +28,30 @@ dataset, _ = make_blobs(
 
 
 ### Create and fit model ###
-print("Creating model")
+logger.info("Creating model")
+logger.debug(
+    f"Model: KMeans, n_clusters={N_CENTERS}, init='k-means++', n_init={N_INIT}"
+)
 model = KMeans(
     n_clusters=N_CENTERS,
     init="k-means++",
     n_init=N_INIT
 )
 
-print("Fitting model")
+logger.info("Fitting model")
 model.fit(dataset)
 labels = model.predict(dataset)
 
 
 ### Score model ###
-print(f"Score: {model.score(dataset)}")
+print(f"Model trained. Score: {model.score(dataset)}")
 
 
 ### Plot clustering ###
 plot_save_path = Path(
     get_output_root_path(), "figures", "clustering", "KNN.png"
 )
-print(f"Plotting image - saving it in {plot_save_path}")
+logger.info(f"Plotting image - saving it in {plot_save_path}")
 
 plt.scatter(
     dataset[:,0], dataset[:,1],
