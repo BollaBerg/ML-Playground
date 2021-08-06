adb84385f6a588fde2bb265469d5b64070d84231
Andreas Bjelland Berg <51373685+BollaBerg@users.noreply.github.com>
2021-08-06 12:35:26 +0200

    Update logging to use logging_config.json
    Also updates get_logger to take in module name, which enables it to create different loggers depending on what file it is in.

--- packages/logging/logging_setup.py
+++ packages/logging/logging_setup.py
@@ -1,29 +1,50 @@
 from pathlib import Path
 import functools
+import json
 import logging
 import logging.config
 
 from packages.path_utility import get_project_root_path
 
-def setup_base_logging():
-    """Setup base logging, including formatting (so logs are kept similar)"""
-    path_to_logging_conf = Path(
-        get_project_root_path(), "packages", "logging", "logging.conf"
+def _setup_base_logging():
+    """Setup base logging, including formatting.
+    
+    Gets setup from {root}/configs/logging_config.json
+    """
+    logging_config = Path(
+        get_project_root_path(), "configs", "logging_config.json"
     )
 
-    logging.config.fileConfig(
-        str(path_to_logging_conf),
-        disable_existing_loggers=False
+    with logging_config.open() as config:
+        config_dict = json.load(config)
+
+    # Deal with log file - ensure path exists
+    path_to_log_file = Path(
+        get_project_root_path(), "logs", "playground_log.log"
     )
+    path_to_log_file.parent.mkdir(parents=True, exist_ok=True)
+    path_to_log_file.touch()
+    
+    config_dict["handlers"]["file"]["filename"] = str(path_to_log_file)
+    
+        
+    logging.config.dictConfig(config_dict)
 
 
-@functools.lru_cache(maxsize=None)
-def get_logger() -> logging.Logger:
-    """Return a base logger, using configuration from logging.conf
+def get_logger(module_name: str) -> logging.Logger:
+    """Create and return a logger with name == module_name
+    
+    Usage:
+        >>> logger = get_logger(__name__)
+        >>> logger.debug("Debug message here")
+
+    Args:
+        module_name (str): Name of module, which will be used as name for the
+            logger. Will usually be __name__
     
     Returns:
         logging.Logger: Configured logger
     """
-    setup_base_logging()
+    _setup_base_logging()
 
-    return logging.getLogger("MLplayground")
+    return logging.getLogger("playground").getChild(module_name)
