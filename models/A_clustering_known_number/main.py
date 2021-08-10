dce300fecfd04a510396696eba94a0f85210acbb
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-08-10 17:13:39 +0200

    Add comparison of models in task A

--- models/A_clustering_known_number/main.py
+++ models/A_clustering_known_number/main.py
@@ -1,14 +1,39 @@
 import data.A_clustering_known_number.generate_data as data
+from packages.path_utility import get_output_root_path, create_and_get_path
+from packages.logging import get_logger
+from packages.plotting.plot_2d import plot_multiple_models
 
 import kmeans
+import agglomerative_ward as ward
+
+logger = get_logger(__name__)
 
 def compare_models():
     """Compare all models defined on task A"""
     dataset = data.get_dataset()
+    labels = dict()
 
+    ### Kmeans ###
+    logger.debug("Working on KMeans")
     kmeans_model, kmeans_score = kmeans.train_and_score_model(dataset)
-
+    labels["KMeans"] = kmeans_model.predict(dataset)
     print(f"KMeans score: {kmeans_score}")
+
+    ### Ward ###
+    logger.debug("Working on Ward")
+    ward_model = ward.create_model()
+    labels["Agglomerative Clustering: Ward"] = ward_model.fit_predict(dataset)
+
+    ### Plotting results ###
+    plot_save_path = create_and_get_path(
+        get_output_root_path(),
+        "figures",
+        "A_clustering_known_number",
+        "COMPARISON.png"
+    )
+    plot_multiple_models(
+        dataset, labels, plot_save_path, "Comparison of task A"
+    )
 
 
 if __name__ == '__main__':
