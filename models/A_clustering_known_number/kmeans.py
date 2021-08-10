24dd160f5fb868a64947e6191be5bc5e5a291721
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-08-10 16:10:55 +0200

    Move functionality in kmeans to functions instead of top-level

--- models/A_clustering_known_number/kmeans.py
+++ models/A_clustering_known_number/kmeans.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
 from sklearn.cluster import KMeans
 
 import data.A_clustering_known_number.generate_data as data
-from packages.path_utility import get_output_root_path
-from packages.logging import logger
+from packages.logging import get_logger
+from packages.path_utility import get_output_root_path, create_and_get_path
+from packages.plotting.plot_2d import plot_2d_results
 
 
 ### CONSTANTS ###
@@ -13,39 +16,45 @@ N_INIT = 15         # Times K-means will be run with different centroid seeds
 
 
 ### SETUP ###
-dataset = data.get_dataset()
-
-### Create and fit model ###
-logger.info("Creating model")
-logger.debug(
-    f"Model: KMeans, n_clusters={data.N_CENTERS}, init='k-means++', "
-    + f"n_init={N_INIT}"
-)
-model = KMeans(
-    n_clusters=data.N_CENTERS,
-    init="k-means++",
-    n_init=N_INIT
-)
-
-logger.info("Fitting model")
-model.fit(dataset)
-labels = model.predict(dataset)
+logger = get_logger(__name__)
 
 
-### Score model ###
-print(f"Model trained. Score: {model.score(dataset)}")
+def create_model() -> sklearn.cluster.KMeans:
+    """Create and return a KMeans-model"""
+    ### Create and fit model ###
+    logger.info("Creating model")
+    logger.debug(
+        f"Model: KMeans, n_clusters={data.N_CENTERS}, init='k-means++', "
+        + f"n_init={N_INIT}"
+    )
+    return KMeans(
+        n_clusters=data.N_CENTERS,
+        init="k-means++",
+        n_init=N_INIT
+    )
 
 
-### Plot clustering ###
-plot_save_path = Path(
-    get_output_root_path(), "figures", "clustering", "KNN.png"
-)
-logger.info(f"Plotting image - saving it in {plot_save_path}")
+def main():
+    dataset = data.get_dataset()
+    model = create_model()
 
-plt.scatter(
-    dataset[:,0], dataset[:,1],
-    c=labels,
-    alpha=0.75
-)
+    ### Fit model ###
+    logger.info("Fitting model")
+    model.fit(dataset)
 
-plt.savefig(plot_save_path)
+    ### Score model ###
+    logger.info(f"Model trained. Score: {model.score(dataset)}")
+
+    ### Plot results ###
+    labels = model.predict(dataset)
+    plot_save_path = create_and_get_path(
+        get_output_root_path(),
+        "figures",
+        "A_clustering_known_number",
+        "kmeans.png"
+    )
+    plot_2d_results(dataset, labels, plot_save_path)
+
+
+if __name__ == "__main__":
+    main()
